740c79affaa254ff81547916c478451fc7c642f2
Koki Kitai <kitai.koki@gmail.com>
2021-06-22 01:43:06 +0900

    Simplify the logging
    The `helper.Logger` class directly read/write to the SQLite3 database.
    Each auxiliary information is stored in a JSON string.

--- src/metabbo/helper/logger.py
+++ src/metabbo/helper/logger.py
@@ -20,85 +20,37 @@
 # IN THE SOFTWARE.
 #===============================================================================
 from   abc import ABC, abstractmethod
+import json
 import os
 import sqlite3
 
-class MetadataType(ABC):
-
-    @classmethod
-    @abstractmethod
-    def sql_formats(cls):
-        '''This method should return a list of element typenames for SQL
-        '''
-        pass
-
-    @abstractmethod
-    def to_strs(self):
-        '''This method should return a list of str exression of all elements
-        '''
-        pass
-
-class NullMetadataType(MetadataType):
-    @classmethod
-    def sql_formats(cls):
-        return []
-
-    def to_strs():
-        return []
-
 class Logger:
-    def __init__(self, x_types, metadata_type):
-        assert issubclass(metadata_type, MetadataType)
-        self.xs = []
-        self.ys = []
-        self.infos = []
+    def __init__(self, x_types, fname=":memory:"):
         self.x_types = x_types
-        self.metadata_type = metadata_type
+        self.x_types_format = ", ".join(map(lambda t: {int: "%d", float: "%e"}[t], x_types))
+
+        #if os.path.exists(fname):
+        #    raise Exception("File {} already exists".format(fname))
+        self.db = sqlite3.connect(fname, timeout=60)
+
+        self.db.execute("CREATE TABLE IF NOT EXISTS Logs ("
+            + ", ".join(["x%d %s"%(n, t.__name__) for (n, t) in enumerate(self.x_types)])
+            + ", y float, info text);")
+
+        self.db.commit()
 
     def _format_x(self, x):
         return [f(el) for (f, el) in zip(self.x_types, x)]
 
-    def log1(self, x, y, info):
-        assert isinstance(info, self.metadata_type)
-        self.xs.append(self._format_x(x))
-        self.ys.append(float(y))
-        self.infos.append(info)
+    def log1(self, x, y, info={}):
+        self.db.execute("INSERT INTO Logs VALUES (" + self.x_types_format%tuple(x) + ", %e, '%s');"%(y, json.dumps(info)))
 
-    def log(self, xs, ys, infos):
-        assert len(xs) == len(ys) and len(xs) == len(infos), "xs, ys, and infos must have the same length."
-        assert all(filter(lambda i: isinstance(i, self.metadata_type), infos))
+    def log(self, xs, ys, infos=None):
+        assert len(xs) == len(ys) and (infos == None or len(xs) == len(infos)), "xs and ys(, and infos) must have the same length."
+        for x, y, info in zip(xs, ys, infos or [{} for _ in range(len(xs))]):
+            self.log1(x, y, info)
 
-        self.xs.extend(map(self._format_x, xs))
-        self.ys.extend(map(float, ys))
-        self.infos.extend(infos)
-
-    def save(self, fname):
-        if os.path.exists(fname):
-            raise Exception("File {} already exists".format(fname))
-        db = sqlite3.connect(fname, timeout=60)
-
-        db.execute("CREATE TABLE Log ("
-            + ", ".join(["x%d %s"%(n, t.__name__) for (n, t) in enumerate(self.x_types)])
-            + ", y float, "
-            + ", ".join(self.metadata_type.sql_formats())
-            + ");")
-
-        for x, y, info in zip(self.xs, self.ys, self.infos):
-            db.execute("INSERT INTO Log VALUES ("
-                + ", ".join(map(lambda el: {int: "%d", float: "%e"}[type(el)]%el, x))
-                + ", %e, " % y
-                + ", ".join(info.to_strs())
-                + ");")
-        db.commit()
-
-    @classmethod
-    def load(cls, fname, x_types, metadata_type):
-        obj = cls(x_types, metadata_type)
-        with sqlite3.connect(fname) as conn:
-            xlen = len(obj.x_types)
-            for xyinfo in conn.execute("SELECT * from Log"):
-                x, y, info = xyinfo[:xlen], xyinfo[xlen], metadata_type(*xyinfo[xlen+1:])
-                obj.log1(x, y, info)
-
-        return obj
+    def __del__(self):
+        self.db.commit()
+        self.db.close()
 
--- test/test_helper_logger.py
+++ test/test_helper_logger.py
@@ -19,7 +19,7 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 #===============================================================================
-from   metabbo.helper.logger import Logger, MetadataType, NullMetadataType
+from   metabbo.helper.logger import Logger
 from   metabbo import FiniteSampling
 
 import logging
@@ -27,82 +27,14 @@ import numpy as np
 import os
 from   unittest import TestCase
 
-class IntIntText(MetadataType):
-
-    @classmethod
-    def sql_formats(cls):
-        return ["n1 int", "n2 int", "s text"]
-
-
-    def __init__(self, n1, n2, s):
-        self.n1 = n1
-        self.n2 = n2
-        self.s = s
-
-    def to_strs(self):
-        return [
-            "%d"%self.n1,
-            "%d"%self.n2,
-            "\"%s\""%self.s
-        ]
-
 class TestLogger(TestCase):
 
-    def test_create(self):
-        logger = Logger([int, int, int, float, float, float], IntIntText)
-
-
     def test_logging(self):
-        logger = Logger([int, int, int, float, float, float], IntIntText)
+        logger = Logger([int, int, int, float, float, float])
         logger.log(
             [    [1,1,1,1.0,1.0,1.0],     [2,2,2,2.0,2.0,2.0]],
             [                    1.0,                     2.0],
-            [IntIntText(1,2,"Hello"), IntIntText(3,4,"World")]
+            [{"index": 0, "order": "first"}, {"index": 1, "order": "second"}]
         )
-        assert len(logger.xs) == 2
-        assert len(logger.ys) == 2
-        assert len(logger.infos) == 2
-        return logger
-
-    def test_save(self):
-        logger = self.test_logging()
-        try:
-            logger.save("test_save.db")
-        except Exception as e:
-            if e.args[0].startswith("File"):
-                logging.info("File already exists. Assuming the test_save had successed before.")
-            else:
-                raise e
-        os.remove("test_save.db")
-
-    def test_load(self):
-        logger = self.test_logging()
-        logger.save("test_load.db")
         del logger
-
-        logger = Logger.load("test_load.db", [int, int, int, float, float, float], IntIntText)
-        assert len(logger.xs) == 2
-        assert len(logger.ys) == 2
-        assert len(logger.infos) == 2
-        assert logger.infos[0].s == "Hello"
-        assert logger.infos[1].s == "World"
-        os.remove("test_load.db")
-
-        return logger
-
-    def test_logger_compat(self):
-        logger = Logger([int, int, int, float, float, float], NullMetadataType)
-
-        try:
-            xs = np.random.randint(5, size=(10,6))
-            FiniteSampling(xs, lambda xs: np.sum(xs, axis=1), num_random_init=5, randseed=123, logger=logger)
-            self.fail()  # An exception should be thrown before this line
-        except AssertionError as ae:
-            if len(ae.args) > 0 and ae.args[0] == None:
-                raise ae
-            else:
-                pass
-
-        xs = [x.tolist() + y.tolist() for x,y in zip(np.random.randint(5, size=(10,3)), np.random.randn(10,3))]
-        FiniteSampling(xs, lambda xs: np.sum(xs, axis=1), num_random_init=5, randseed=123, logger=logger)
 
